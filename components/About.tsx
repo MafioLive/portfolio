8ee124400a6e1761020dee79b1e7e35c59b9a97e
Wesley <Wesleyrose06@gmail.com>
2022-12-07 15:36:02 -0500

    update about me

--- components/About.tsx
+++ components/About.tsx
@@ -10,35 +10,25 @@ export const About: NextPage = () => {
       <div className="about_left">
         <h1 className="about_left_head">Hi, I&apos;m Wesley Rose!</h1>
         <p className="about_left_text">
-        Hi! I am Wesley Rose from Ontario, Canada. I am Self Taught Software/Network Engineer.
+        Hi! I am Wesley Rose from Ontario, Canada. I am Software/Network Engineer.
         </p>
         <div className="about_left_socialMedia">
           <img
             src={github.src}
-            alt=""
+            alt="Link to github"
             onClick={() => window.open("https://github.com/MafioLive", "_blank")}
           />
-          {/* <img
-            src={linkedin.src}
-            alt=""
-            onClick={() => window.open("https://www.linkedin.com/in/himankash/", "_blank")}
-          />
-          <img
-            src={instagram.src}
-            alt=""
-            onClick={() => window.open("https://www.instagram.com/himan_kash/", "_blank")}
-          /> */}
           <img
             src={gmail.src}
-            alt=""
+            alt="Send Me a EMAIL"
             onClick={() => window.open("mailto:wesleyrose06@gmail.com", "_blank")}
           />
         </div>
         <div
-          className="about_left_starme"
+          className="about_left_followme"
           onClick={() => window.open("https://github.com/MafioLive", "_blank")}
         >
-          ⭐ Follow me on GitHub
+          Follow me on GitHub
         </div>
       </div>
       <div className="about_right">
