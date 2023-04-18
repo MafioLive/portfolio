db65ece010a8093a3d1d803941f8e8d7d5a4ec9e
Wesley <Wesleyrose06@gmail.com>
2023-04-18 15:27:05 -0400

    Remove the Lorem Ipsum

--- src/data/headerData.js
+++ src/data/headerData.js
@@ -3,7 +3,7 @@ import resume from '../assets/pdf/resume.pdf'
 export const headerData = {
     name: 'Wesley Rose',
     title: "Full-Stack Developer",
-    desciption:"Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua.",
+    desciption:"Unlock the power of innovation in my portfolio, featuring cutting-edge technologies and creative problem-solving that brings the world to life through code.",
     image: 'https://media.graphassets.com/output=format:jpg/resize=width:59,height:59,fit:crop/lqkPdYySSmJsuJYdlBoe',
     resumePdf: resume
 }
