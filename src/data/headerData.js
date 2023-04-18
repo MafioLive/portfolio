594c4bf4c9598838bb3ed24101bfb66b6707415b
Wesley <Wesleyrose06@gmail.com>
2023-04-18 15:28:43 -0400

    fix grammar

--- src/data/headerData.js
+++ src/data/headerData.js
@@ -3,7 +3,7 @@ import resume from '../assets/pdf/resume.pdf'
 export const headerData = {
     name: 'Wesley Rose',
     title: "Full-Stack Developer",
-    desciption:"Unlock the power of innovation in my portfolio, featuring cutting-edge technologies and creative problem-solving that brings the world to life through code.",
+    desciption:"Unlock the power of innovation through my portfolio, featuring cutting-edge technologies and creative problem-solving that brings the world to life through code.",
     image: 'https://media.graphassets.com/output=format:jpg/resize=width:59,height:59,fit:crop/lqkPdYySSmJsuJYdlBoe',
     resumePdf: resume
 }
