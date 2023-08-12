e5e4a456e8cb94fad2d1798e05edfc29c0faf2a3
Wesley <48069938+MafioLive@users.noreply.github.com>
2023-08-12 17:57:59 -0400

    Update headerData.js

--- src/data/headerData.js
+++ src/data/headerData.js
@@ -4,6 +4,6 @@ export const headerData = {
     name: 'Wesley Rose',
     title: "Full-Stack Developer",
     desciption:"Through my portfolio you can unlock the power of innovation, featuring cutting-edge technologies and creative problem-solving that brings the world to life through code.",
-    image: 'https://media.graphassets.com/output=format:jpg/resize=width:59,height:59,fit:crop/lqkPdYySSmJsuJYdlBoe',
+    image: 'https://media.graphassets.com/output=format:jpg/resize=width:200,height:200/tThYeT6WT5mv6Rsop6Qr',
     resumePdf: resume
 }
